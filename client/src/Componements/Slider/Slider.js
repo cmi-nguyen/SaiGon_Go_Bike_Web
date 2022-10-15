c323f820cc66247f0d76a0d8b4b381a4ab344f7d
Nguyn Trieu <67179509+cmi-nguyen@users.noreply.github.com>
2022-10-15 14:29:46 +0700

    Change slider styling

--- client/src/Componements/Slider/Slider.js
+++ client/src/Componements/Slider/Slider.js
@@ -1,31 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Carousel,
   CarouselItem,
   CarouselControl,
   CarouselIndicators,
   CarouselCaption,
-} from 'reactstrap';
- import "../Slider/Slider.css"
-
+} from "reactstrap";
+import "../Slider/Slider.css";
 
 const items = [
   {
-    src: 'https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-6/279053634_117872530887508_8423480886810105089_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_ohc=xpdcZf10mksAX8m7RQi&_nc_ht=scontent.fsgn5-6.fna&oh=00_AT-kfOnLhVrWoBwvSDi1NJyivis733GCLgwioIxS7FXyow&oe=634F0321',
-    altText: 'Slide 1',
-    caption: 'Caption Slide 1',
+    src: "https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-6/279053634_117872530887508_8423480886810105089_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=e3f864&_nc_ohc=xpdcZf10mksAX8m7RQi&_nc_ht=scontent.fsgn5-6.fna&oh=00_AT-kfOnLhVrWoBwvSDi1NJyivis733GCLgwioIxS7FXyow&oe=634F0321",
+    altText: "Slide 1",
+    caption: "Caption Slide 1",
     key: 1,
   },
   {
-    src: 'https://picsum.photos/id/456/1200/400',
-    altText: 'Slide 2',
-    caption: 'Caption Slide 2',
+    src: "https://picsum.photos/id/456/1200/400",
+    altText: "Slide 2",
+    caption: "Caption Slide 2",
     key: 2,
   },
   {
-    src: 'https://picsum.photos/id/678/1200/400',
-    altText: 'Slide 3',
-    caption: 'Caption Slide 3',
+    src: "https://picsum.photos/id/678/1200/400",
+    altText: "Slide 3",
+    caption: "Caption Slide 3",
     key: 3,
   },
 ];
@@ -59,7 +58,7 @@ function Slider() {
         key={item.src}
       >
         <img src={item.src} alt={item.altText} />
-        <CarouselCaption 
+        <CarouselCaption
           captionText={item.altText}
           captionHeader={item.caption}
         />
@@ -68,29 +67,26 @@ function Slider() {
   });
 
   return (
-    <Carousel
-      activeIndex={activeIndex}
-      next={next}
-      previous={previous}
-      
-    >
-      <CarouselIndicators
-        items={items}
-        activeIndex={activeIndex}
-        onClickHandler={goToIndex}
-      />
-      {slides}
-      <CarouselControl
-        direction="prev"
-        directionText="Previous"
-        onClickHandler={previous}
-      />
-      <CarouselControl
-        direction="next"
-        directionText="Next"
-        onClickHandler={next}
-      />
-    </Carousel>
+    <div className="slider">
+      <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+        <CarouselIndicators
+          items={items}
+          activeIndex={activeIndex}
+          onClickHandler={goToIndex}
+        />
+        {slides}
+        <CarouselControl
+          direction="prev"
+          directionText="Previous"
+          onClickHandler={previous}
+        />
+        <CarouselControl
+          direction="next"
+          directionText="Next"
+          onClickHandler={next}
+        />
+      </Carousel>
+    </div>
   );
 }
 
